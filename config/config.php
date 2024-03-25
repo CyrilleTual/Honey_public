ff243fa0b53042e22d587831e36271d19dfa53b9
cyrilletual <cyrille.tual@gmail.com>
2024-03-25 14:24:28 +0100

    update github tracking

--- config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-// pour mail sur erreur grave ( page errorsWebSite.phtml)
-const ADMIN_ADDRESS = '************************';
-const ADMIN_ID = '************';
-
-?>
